76cebc483c169dd3cc2410cbd871b93541e21fc0
ram <ramireddy251@gmail.com>
2026-03-10 14:02:22 +0530

    roboshop infra

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -22,6 +22,11 @@ data "aws_ssm_parameter" "redis_sg_id" {
 
 }
 
+data "aws_ssm_parameter" "mysql_sg_id" {
+    name = "/${var.project}/${var.environment}/mysql_sg_id"
+
+}
+
 data "aws_ssm_parameter" "catalogue_sg_id" {
     name = "/${var.project}/${var.environment}/catalogue_sg_id"
 
