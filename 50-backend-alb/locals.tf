50c9878741dc3dcddb87cc1c8d07e78979d1b56c
ram <ramireddy251@gmail.com>
2026-03-10 23:16:25 +0530

    roboshop infra

--- 50-backend-alb/locals.tf
+++ 50-backend-alb/locals.tf
@@ -4,7 +4,7 @@ locals {
         environment = var.environment
         Terraform = "true"
     }
-    backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg.value
+    backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
     private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
     route53_zone_id = data.aws_ssm_parameter.route53_zone_id.value
 
