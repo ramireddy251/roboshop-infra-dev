4233ec1cde23d0643a762ffbea8417575556012c
ram <ramireddy251@gmail.com>
2026-03-10 16:37:14 +0530

    roboshop infra

--- 40-databases/iam.tf
+++ 40-databases/iam.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_role" "mysql" {
     name = local.mysql_role_name
 
-    assume_role_policy = jsoncode({
+    assume_role_policy = jsonencode({
         Version = "2012-10-17"
         Statement = [
             {
