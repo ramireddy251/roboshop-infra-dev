c1eb662411cf4ba24fbb16c2e456e207aac7cf67
ram <ramireddy251@gmail.com>
2026-03-10 23:03:43 +0530

    roboshop infra

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -41,3 +41,8 @@ data "aws_ssm_parameter" "user_sg_id" {
     name = "/${var.project}/${var.environment}/user_sg_id"
 
 }
+
+data "aws_ssm_parameter" "backend_alb_sg_id" {
+    name = "/${var.project}/${var.environment}/backend_alb_sg_id"
+
+}
