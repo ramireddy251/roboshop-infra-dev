60682788e0e21669e6fb69f2ba792a3a5bd9d422
ram <ramireddy251@gmail.com>
2026-03-11 14:40:43 +0530

    roboshop infra

---
+++ 60-catalogue/locals.tf
@@ -0,0 +1,9 @@
+locals {
+    common_tags = {
+        project = var.project
+        environment = var.environment
+        Terraform = "true"
+    }
+    catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
+    private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+}
---
+++ 60-catalogue/main.tf
@@ -0,0 +1,38 @@
+resource "aws_instance" "catalogue" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    subnet_id = local.private_subnet_id
+    vpc_security_group_ids = [local.catalogue_sg_id]
+
+    tags = merge(
+        {
+            Name = "${var.project}-${var.environment}-catalogue"
+        },
+        local.common_tags
+    )
+}
+
+resource "terraform_data" "bootstrap_catalogue" {
+    triggers_replace = [
+        aws_instance.catalogue.id
+    ]
+
+    connection {
+        type = "ssh"
+        user = "ec2-user"
+        password = "DevOps321"
+        host = aws_instance.catalogue.private_ip
+  }
+
+    provisioner "file" {
+        source = "bootstrap.sh"
+        destination = "/tmp/bootstrap.sh"
+    }
+
+    provisioner "remote-exec" {
+       inline = [
+          "chmod +x /tmp/bootstrap.sh",
+          "sudo sh /tmp/bootstrap.sh catalogue ${var.environment}"
+       ]      
+    }
+}
