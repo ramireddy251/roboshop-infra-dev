a6dbfa3e5d2efeb51f5cdd6a7a57fb0c74a5c4eb
ram <ramireddy251@gmail.com>
2026-03-12 13:12:18 +0530

    roboshop infra

--- 50-backend-alb/parameters.tf
+++ 50-backend-alb/parameters.tf
@@ -1,5 +1,4 @@
 resource "aws_ssm_parameter" "backend_alb_listener_arn" {
-    count = length(var.sg_names)
     name = "/${var.project}/${var.environment}/backend_alb_listener_arn"
     type = "String"
     value = aws_lb_listener.http.arn
