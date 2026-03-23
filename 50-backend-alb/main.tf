2fa17d0c78c10d062e29a44d3eef458f29d68010
ram <ramireddy251@gmail.com>
2026-03-23 13:42:20 +0530

    roboshop infra

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -25,7 +25,7 @@ resource "aws_lb_listener" "http" {
 
     fixed_response {
       content_type = "text/html"
-      message_body = "<h1>Hi, I am from backend ALB</h1>"
+      message_body = "<h1>Hi, I am HTTP from backend ALB</h1>"
       status_code  = "200"
     }
   }
