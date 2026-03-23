698d72194744a22dc4e8bbaa3c5218098200e337
ram <ramireddy251@gmail.com>
2026-03-23 17:26:26 +0530

    roboshop infra

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -33,7 +33,7 @@ resource "aws_lb_listener" "https" {
   }
 }
 
-resource "aws_route53_record" "backend-alb" {
+resource "aws_route53_record" "frontend-alb" {
   zone_id = local.route53_zone_id
   name    = "*.${var.domain_name}"
   type    = "A"
